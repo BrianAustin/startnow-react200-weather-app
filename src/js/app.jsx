733234bfe4e8ad9c67b0a9b92ce52604e2d9e33a
Brian <baustin27@gmail.com>
2018-06-10 11:56:33 -0700

    styling and layout

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -84,7 +84,25 @@ export default class App extends React.Component {
             {/* City Info box end above */}
             {/* Search History box start below */}
             <div className='col-12 col-md-6 mb-4'>
-              
+              <div className='card border-primary mb-3'>
+                <div className='card-header text-white bg-primary'>Search History</div>
+                <div className='card-body'>
+                <table className='table table-sm table-hover'>
+                  <thead>
+                    <tr>
+                      <th>City</th>
+                      <th>Date</th>
+                    </tr>  
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>city</td>
+                      <td><p>date</p><p>time</p></td>
+                    </tr>  
+                  </tbody>    
+                </table>
+                </div>
+              </div>
             </div>
             {/* Search History box end above */}
           {/* closing row div for CITY INFO and SEARCH HISTORY below   */}
